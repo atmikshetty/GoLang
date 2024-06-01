4f6c46ecf63cab2967c66ba124fc6a3493be4c65
atmikshetty <atmikshetty10@student.sfit.ac.in>
2024-06-01 12:26:18 +0530

    Variables and Public and Private

--- main.go
+++ main.go
@@ -1,12 +1,30 @@
 package main
 
 import (
-	myutil "atmik/functions"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("Hello Atmik Shetty.")
 
-	myutil.PrintMessage("Hello")
+	var name string = "Atmik"
+	var age = 18
+	fmt.Println(name)
+	fmt.Print(age)
+
+	var dimensions float64 = 90.876
+	fmt.Println(dimensions)
+
+	var claim bool = true
+	claim = true
+	fmt.Print(claim)
+
+	const pi = 3.14
+	fmt.Println(pi)
+
+	goat := "Messi"
+	fmt.Print(goat)
+
+	// Starting with capital letter: Public can be used anywhere
+	// Starting with small letter: private can be used only within the file
 }
